19fd06ac7c744d269da8587a4ddacfaa31385fc3
spences10 <spences10apps@gmail.com>
2017-04-19 07:06:56 +0100

    access dispatch and state with redux 🦆

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,4 +1,5 @@
-
+// every reducer runs every time there is an action
+// what you do with that action is up to you 👍
 function comments(state = [], action) {
   console.log(state, action)
   return state
