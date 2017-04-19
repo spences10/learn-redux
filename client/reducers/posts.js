f7c3fe1e95ff32020b770b7b03ce17e5fa9c3435
spences10 <spences10apps@gmail.com>
2017-04-19 11:04:16 +0100

    updating state with reducers :straight_ruler:

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -5,8 +5,20 @@
 
 // set state to be default of empty along with the action
 function posts(state = [], action) {
-  console.log(state, action)
-  return state
+  switch (action.type) {
+    case 'INCREMENT_LIKES' :
+      console.log('incrementing likes!!')
+      const i = action.index
+      // update the state
+      return [
+        ...state.slice(0, i), // before the post we're updating
+        {...state[i], likes: state[i].likes + 1}, // copy the object and add new number of likes
+        ...state.slice(i + 1) // after the post we're updating
+      ]  
+    // return the updated state
+    default:
+      return state
+  }
 }
 
 export default posts
